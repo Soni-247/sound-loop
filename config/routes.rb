bfa6539b49c4e310f96d1520fc87fd7e29418c3a
Soni-247 <sonifisi@Gmail.com>
2020-11-18 01:15:15 +1100

    Added gems and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :listings
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "listings#index"
+
 end
